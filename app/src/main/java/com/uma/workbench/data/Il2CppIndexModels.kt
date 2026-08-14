73af9ceb2a133a1a2556c1933a1d01c3c255b755
xf8410 <1421506586@qq.com>
2026-08-14 20:00:22 +0800

    feat: add persistent IL2CPP section and string fragment indexes

---
+++ app/src/main/java/com/uma/workbench/data/Il2CppIndexModels.kt
@@ -0,0 +1,33 @@
+package com.uma.workbench.data
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+
+@Entity(
+    tableName = "il2cpp_sections",
+    primaryKeys = ["sourceId", "name"],
+    indices = [Index("sourceId")]
+)
+data class Il2CppSectionEntity(
+    val sourceId: String,
+    val name: String,
+    val offset: Long,
+    val byteCount: Long,
+    val metadataVersion: Int,
+    val rangeValid: Boolean
+)
+
+@Entity(
+    tableName = "il2cpp_string_fragments",
+    primaryKeys = ["sourceId", "offset"],
+    indices = [Index("sourceId"), Index(value = ["sourceId", "text"])]
+)
+data class Il2CppStringFragmentEntity(
+    val sourceId: String,
+    val offset: Long,
+    val byteCount: Int,
+    val text: String,
+    val continuesFromPrevious: Boolean,
+    val continuesToNext: Boolean
+)
