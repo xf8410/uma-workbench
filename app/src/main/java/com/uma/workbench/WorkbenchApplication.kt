829831131d46fb5a250a8cf68e06bb39e9c4bc06
xf8410 <1421506586@qq.com>
2026-08-14 17:54:12 +0800

    feat: provide work scheduler from application container

--- app/src/main/java/com/uma/workbench/WorkbenchApplication.kt
+++ app/src/main/java/com/uma/workbench/WorkbenchApplication.kt
@@ -5,20 +5,19 @@ import com.uma.workbench.data.AppDatabase
 import com.uma.workbench.data.WorkbenchRepository
 import com.uma.workbench.imports.SourceImporter
 import com.uma.workbench.network.NetworkMonitor
+import com.uma.workbench.worker.WorkScheduler
 
 class WorkbenchApplication : Application() {
-    lateinit var repository: WorkbenchRepository
-        private set
-    lateinit var networkMonitor: NetworkMonitor
-        private set
-    lateinit var sourceImporter: SourceImporter
-        private set
+    lateinit var repository: WorkbenchRepository; private set
+    lateinit var networkMonitor: NetworkMonitor; private set
+    lateinit var sourceImporter: SourceImporter; private set
+    lateinit var workScheduler: WorkScheduler; private set
 
     override fun onCreate() {
         super.onCreate()
-        val database = AppDatabase.get(this)
-        repository = WorkbenchRepository(database)
+        repository = WorkbenchRepository(AppDatabase.get(this))
         networkMonitor = NetworkMonitor(this)
         sourceImporter = SourceImporter(contentResolver)
+        workScheduler = WorkScheduler(this)
     }
 }
