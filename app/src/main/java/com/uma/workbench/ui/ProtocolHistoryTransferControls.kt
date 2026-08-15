c1b45be7ad5b8ca9d4bb7ab31931c69486d999eb
xf8410 <1421506586@qq.com>
2026-08-15 13:38:05 +0800

    Fix RowScope weight resolution in SAF controls

--- app/src/main/java/com/uma/workbench/ui/ProtocolHistoryTransferControls.kt
+++ app/src/main/java/com/uma/workbench/ui/ProtocolHistoryTransferControls.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
